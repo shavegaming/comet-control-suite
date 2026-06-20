5ab33ecda7932cf9872da80f2fec8739d8277b5f
𝓢𝓱𝓪𝓿𝓮 <nevergonnagiveyouupjoemam@gmail.com>
2026-06-20 16:24:54 -0700

    Initialize Discord bot with command handling

---
+++ bot/src/index.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const { Client, GatewayIntentBits, REST, Routes } = require('discord.js');
+const fs = require('fs');
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const commands = [];
+const commandFiles = fs.readdirSync('./src/commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    commands.push(command.data.toJSON());
+    client.commands.set(command.data.name, command);
+}
+
+client.once('ready', async () => {
+    console.log(`🤖 Bot online as ${client.user.tag}`);
+    const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
+    await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: commands });
+});
+
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+    const command = client.commands.get(interaction.commandName);
+    if (command) await command.execute(interaction);
+});
+
+client.login(process.env.DISCORD_TOKEN);
